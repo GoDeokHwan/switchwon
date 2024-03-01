935973469e6e1874388c0cc225f45a0c6e809ff0
고덕환 <dhdh9224@gmail.com>
2024-03-01 09:55:49 +0900

    init

---
+++ switchwon-api/src/main/java/com/example/switchwonapi/SwitchwonApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.switchwonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwitchwonApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwitchwonApiApplication.class, args);
+    }
+
+}
---
+++ switchwon-api/src/test/java/com/example/switchwonapi/SwitchwonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.switchwonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwitchwonApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
